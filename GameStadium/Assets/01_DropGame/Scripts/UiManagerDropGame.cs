e877b36e2f73efdec2935b7253eb9e57cc1e7957
Philipp Kalenda <philipp.kalenda@lieberlieber.com>
2019-01-31 02:24:43 +0100

    GS-6 DifficultyManager

--- GameStadium/Assets/01_DropGame/Scripts/UiManagerDropGame.cs
+++ GameStadium/Assets/01_DropGame/Scripts/UiManagerDropGame.cs
@@ -1,10 +1,15 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+
+using Difficulty = Assets._01_DropGame.Scripts.GameManagerDropGame.Difficulty;
 
 namespace Assets._01_DropGame.Scripts
 {
 	public class UiManagerDropGame : MonoBehaviour
 	{
+		public TextMeshProUGUI difficultyLabel;
+
 		TextMeshProUGUI _dropCountText;
 
 		private void Awake()
@@ -15,6 +20,11 @@ namespace Assets._01_DropGame.Scripts
 		public void UpdateDropCount(int count)
 		{
 			_dropCountText.text = count.ToString();
+		}
+
+		public void PrintDifficulty(Difficulty gameDifficulty)
+		{
+			difficultyLabel.text = Enum.GetName(typeof(Difficulty), gameDifficulty);
 		}
 	}
 }
